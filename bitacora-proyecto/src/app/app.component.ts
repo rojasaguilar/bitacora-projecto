6187140e0ebcd24964907d3316a88263f58f34b6
rojasaguilar <rojas.aguilar.ramses721@gmail.com>
2021-06-01 12:55:04 -0500

    cursores correctos

--- bitacora-proyecto/src/app/app.component.ts
+++ bitacora-proyecto/src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bitacora-proyecto';
+
   constructor(
     private _router: Router
   ) {
     
   }
-
+  ngOnInit(){
+    $('.links').mouseover(function(){
+      $('.links').css('cursor','pointer'); 
+  });
+}
+  redireccionarHome(){
+    return this._router.navigate(['']);
+  }
   redireccionarTecnologias(){
     return this._router.navigate(['/tecnologias']);
   
