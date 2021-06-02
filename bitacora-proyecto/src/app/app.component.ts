2153eb5b5054dff7915ed84ee6dfc15bf14a45dc
KevinJPerezD <vi12978@innovaccion.mx>
2021-06-02 14:34:15 -0600

    cambios kevin

--- bitacora-proyecto/src/app/app.component.ts
+++ bitacora-proyecto/src/app/app.component.ts
@@ -5,39 +5,15 @@ declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'bitacora-proyecto';
 
-  constructor(
-    private _router: Router
-  ) {
-    
-  }
-  ngOnInit(){
-    $('.links').mouseover(function(){
-      $('.links').css('cursor','pointer'); 
-  });
-}
-  redireccionarHome(){
-    return this._router.navigate(['']);
-  }
-  redireccionarTecnologias(){
-    return this._router.navigate(['/tecnologias']);
-  
-  }
-  redireccionarServidor(){
-    return this._router.navigate(['/servidor']);
-  
-  }
-  redireccionarNosotros(){
-    return this._router.navigate(['/nosotros']);
-  
-  }
-  redireccionarCodigo(){
-    return this._router.navigate(['/codigo']);
-  
+  constructor(private _router: Router) {}
+  ngOnInit() {
+    $('.links').mouseover(function () {
+      $('.links').css('cursor', 'pointer');
+    });
   }
 }
-
